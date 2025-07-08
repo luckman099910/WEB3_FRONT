3c0dd5063c12502ddf19e5af43f562c9c2982d89
Joun <joun3910@gmail.com>
2025-07-09 04:20:37 +0900

    console

--- src/api/loginApi.ts
+++ src/api/loginApi.ts
@@ -2,5 +2,7 @@ import { api } from './palmPayApi';
 
 export const login = async (email: string, password: string) => {
   const response = await api.post('/api/auth/login', { email, password });
+  console.log(response);
+  
   return response.data;
 }; 
